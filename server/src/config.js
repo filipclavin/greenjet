0a87a2f8b3ce17f5cc924bd62208e733a0e4bbe1
filipclavin <filip.clavin@student.kyh.se>
2021-11-03 09:53:51 +0100

    set up node server

---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
+// Export env variables
+module.exports = {
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+};
---
+++ server/src/router.js
@@ -0,0 +1,7 @@
+const { API_KEY, API_SECRET } = require("./config");
+const Amadeus = require("amadeus");
+const express = require("express");
+// Create router
+const router = express.Router();
+// ...
+module.exports = router;
