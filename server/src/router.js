44a345e5b1fe088ccc57de88c5921ce245eda2e4
filipclavin <filip.clavin@student.kyh.se>
2021-11-03 10:02:52 +0100

    amadeus api client created

--- server/src/router.js
+++ server/src/router.js
@@ -3,5 +3,10 @@ const Amadeus = require("amadeus");
 const express = require("express");
 // Create router
 const router = express.Router();
+// Create Amadeus API client
+const amadeus = new Amadeus({
+    clientId: API_KEY,
+    clientSecret: API_SECRET,
+});
 // ...
 module.exports = router;
